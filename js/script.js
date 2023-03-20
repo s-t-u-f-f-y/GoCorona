2fd4a85c794a4772bdc954c86bb3a921e923b0aa
s-t-u-f-f-y <110378961+s-t-u-f-f-y@users.noreply.github.com>
2023-03-20 19:14:39 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,4 +25,28 @@ function parallax(e){
     });
 }
 
+const goTopBtn = document.querySelector(".go-top");
 
+window.addEventListener("scroll", scrollEvent);
+goTopBtn.addEventListener("click", goTop);
+
+function scrollEvent(e){
+    const scrolled = window.pageYOffset;
+
+    const coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+        // кнопка появляется
+        goTopBtn.classList.add("_show");
+      } else {
+        // иначе исчезает
+        goTopBtn.classList.remove("_show");
+      }
+}
+
+function goTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -25); 
+      setTimeout(goTop, 0); 
+    }
+}
